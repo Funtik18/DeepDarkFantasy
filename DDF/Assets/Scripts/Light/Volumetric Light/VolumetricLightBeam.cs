5b9f79f1e925361bf14a59255fe3e013f80ad8bd
Funtik18 <sir.funtik18@yandex.ru>
2020-07-29 16:39:11 +0700

    Color space - linear

---
+++ DDF/Assets/Scripts/Light/Volumetric Light/VolumetricLightBeam.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Light.VolumentricLight {
+
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    public partial class VolumetricLightBeam : MonoBehaviour {
+        
+    }
+
+}
+
