b35d48f9d5815b75545db7377ff5e1a01acee582
VagueFuture <igrokart97@mail.ru>
2020-08-04 18:34:55 +0700

    Archers

---
+++ DDF/Assets/Resources/Weapon/Bow/Arrow/arrow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int dmg = 10;
+    public int countInScene = 2;
+    public GameObject plumage;
+    private bool fly = true;
+    private float _timer = 0;
+    void Start()
+    {
+    }
+    
+    private void Update() {
+        _timer+=Time.deltaTime;
+        if(_timer>20){
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(fly){
+            Character_stats cS = other.gameObject.GetComponent<Character_stats>();
+            if(cS != null)
+                cS.getHit(dmg,gameObject.transform.root.gameObject);
+            plumage.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.transform.root.parent = other.transform;
+            fly = false;
+        }
+    }
+}
---
+++ DDF/Assets/Resources/Weapon/Bow/bow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator myanim;
+    public GameObject arrow;
+    public Transform bowstring;
+    public bool startPrepare = false,ready = false,shoot = false;
+    private GameObject arW;
+    void Start()
+    {
+        myanim = this.GetComponent<Animator>();
+    }
+
+    private void Update() {
+        myanim.SetBool("ready",ready);
+        myanim.SetBool("shoot",shoot);
+
+        if(startPrepare){
+            startPrepare = false;
+            prepare();
+            ready = true;
+        }
+        if(shoot && ready){
+            Fire();
+            ready = false;
+        }else{
+            shoot = false;
+        }
+    }
+    // Update is called once per frame
+    public void prepare(){
+        arW = Instantiate(arrow, bowstring.position, bowstring.rotation);
+        arW.transform.parent = bowstring;
+    }
+
+    public void Fire(){
+        arW.transform.parent = null;
+        arW.GetComponent<Rigidbody>().isKinematic = false;
+        arW.GetComponent<Rigidbody>().AddForce(transform.right*5000);
+        myanim.SetBool("shoot",shoot);
+        shoot = false;
+    }
+
+}
