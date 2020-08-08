c6d81e42d069e20d015e4de3980c8a8757f588c3
Funtik18 <sir.funtik18@yandex.ru>
2020-08-08 23:50:36 +0700

    prepare

--- DDF/Assets/Scripts/GUI/UI/BarsSystem/BarBase.cs
+++ DDF/Assets/Scripts/GUI/UI/BarsSystem/BarBase.cs
@@ -2,7 +2,28 @@
 using UnityEngine;
 
 namespace DDF.UI.Bar {
+    [RequireComponent(typeof(Image))]
     public class BarBase : MonoBehaviour{
-        public Image;
-    }
+        private Image imageBar;
+
+		public float CurrentCount { 
+			get { return imageBar.fillAmount; }
+			set {
+				if (value <= 0) value = 0;
+				if (value >= 1) value = 1;
+				imageBar.fillAmount = value;
+			}
+		}
+
+		private void Awake() {
+			imageBar = GetComponent<Image>();
+		}
+
+		protected void IncreaseOn(float val) {
+			CurrentCount += val;
+		}
+		protected void DecreaseOn( float val ) {
+			CurrentCount -= val;
+		}
+	}
 }
---
+++ DDF/Assets/Scripts/GUI/UI/BarsSystem/HealthBar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DDF.UI.Bar {
+    
+    public class HealthBar : BarBase {
+
+        /// <summary>
+        /// События, если вышел за минимум.
+        /// </summary>
+        [HideInInspector] public UnityAction beyondMin;
+
+        /// <summary>
+        /// Событие, если преодалел максимум.
+        /// </summary>
+        [HideInInspector] public UnityAction overMax;
+
+        /// <summary>
+        /// Событие, если преодалел максимум повторно.
+        /// </summary>
+        [HideInInspector] public UnityAction overMaxLastMax;
+        private int amount = 0;
+
+
+        public void TakeDamage(float damage) {
+            float normalized = 1f / damage;
+
+            DecreaseOn(normalized);
+		    
+            if(CurrentCount == 0) {
+                beyondMin?.Invoke();
+            }
+
+            amount = 0;
+        }
+        public void RestoreHealth( float health ) {
+            float normalized = 1f / health;
+
+			if (amount > 0) {
+                overMaxLastMax?.Invoke();
+            }
+
+            IncreaseOn(normalized);
+
+            if (CurrentCount == 1) {
+                overMax?.Invoke();
+                amount++;
+            }
+        }
+    }
+}
