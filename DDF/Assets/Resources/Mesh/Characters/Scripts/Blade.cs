469b422903511f38fce3e8895d8c531f2272c81b
Funtik18 <sir.funtik18@gmail.com>
2020-09-08 22:34:31 +0700

    Update Blade.cs

--- DDF/Assets/Resources/Mesh/Characters/Scripts/Blade.cs
+++ DDF/Assets/Resources/Mesh/Characters/Scripts/Blade.cs
@@ -2,9 +2,8 @@
 using DDF.Character.Stats;
 using UnityEngine;
 
-public class Blade : MonoBehaviour
-{
-    public int dmg = 1;
+public class Blade : MonoBehaviour {
+    public float dmg = 1;
     public bool active;
     public float koef = 10f;
     private Vector3 oldPos;
@@ -12,40 +11,51 @@ public class Blade : MonoBehaviour
     private bool speedOrder;
     public float moveSpeed;
 
-    // Start is called before the first frame update
     private void OnTriggerEnter(Collider other) {
-        NPSEntity ncS = other.gameObject.GetComponent<NPSEntity>();
-        CharacterEntity cS = other.gameObject.GetComponent<CharacterEntity>();
+        Entity entity;
+
+        entity = other.gameObject.GetComponent<Entity>();
         string myname = gameObject.transform.root.name;
         string hisname = other.name;
-        if(myname != hisname)
-            if(active){
-                if(moveSpeed>1){
-                    if(ncS != null){
-                        ncS.TakeDamage(moveSpeed%dmg);
-                        //cS.TakeDamage((int)moveSpeed%dmg,gameObject.transform.root.gameObject);
-                    } 
-                    if(cS != null){
-                        cS.TakeDamage(moveSpeed%dmg);
-                        //cS.TakeDamage((int)moveSpeed%dmg,gameObject.transform.root.gameObject);
-                    }     
-                } 
+        if (myname != hisname)
+            if (active) {
+                if (moveSpeed > 1) {
+                    entity?.TakeDamage(moveSpeed % dmg);
+                }
             }
     }
 
-    private void Start(){
-        if(gameObject.transform.root.GetComponent<NPSEntity>() != null)
-        dmg = (int)gameObject.transform.root.GetComponent<NPSEntity>().GetMeleeDamage();
-        if(dmg<=0)
+    private void Start() {
+        Entity currentEntity = gameObject.transform.root.GetComponent<Entity>();
+        if(currentEntity)
+            dmg = currentEntity.GetMeleeDamage();
+        if (dmg <= 0)
             dmg = 1;
     }
-       
-void FixedUpdate () {
-     if (speedOrder) {
+
+    void FixedUpdate() {
+        if (speedOrder) {
             newPos = transform.position;
             moveSpeed = Vector3.Distance(oldPos, newPos) * koef;
-     } else
+        } else
             oldPos = transform.position;
-            speedOrder = !speedOrder;          
+        speedOrder = !speedOrder;
+    }
 }
-}
+/*NPSEntity ncS = other.gameObject.GetComponent<NPSEntity>();
+               CharacterEntity cS = other.gameObject.GetComponent<CharacterEntity>();
+               string myname = gameObject.transform.root.name;
+               string hisname = other.name;
+               if(myname != hisname)
+                   if(active){
+                       if(moveSpeed>1){
+                           if(ncS != null){
+                               ncS.TakeDamage(moveSpeed%dmg);
+                               //cS.TakeDamage((int)moveSpeed%dmg,gameObject.transform.root.gameObject);
+                           } 
+                           if(cS != null){
+                               cS.TakeDamage(moveSpeed%dmg);
+                               //cS.TakeDamage((int)moveSpeed%dmg,gameObject.transform.root.gameObject);
+                           }     
+                       } 
+                   }*/
