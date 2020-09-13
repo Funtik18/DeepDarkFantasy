eeb1a0cdbf7ccf6e38da6cb1c210faba645aae10
Aleksey (dDmioko) <aleksejj.anisimov@gmail.com>
2020-09-13 12:29:01 +0700

    Rework camera control

--- DDF/Assets/Scripts/Camera/CameraConfig.cs
+++ DDF/Assets/Scripts/Camera/CameraConfig.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Camera/Config")]
 public class CameraConfig : ScriptableObject
 {
-    public float turnSmooth;
-    public float pivotSpeed;
-    public float Y_rot_speed;
-    public float X_rot_speed;
-    public float minAngle;
-    public float maxAngle;
-    public float normalZ;
-    public float normalX;
-    public float aimZ;
-    public float aimX;
-    public float normalY;
+    public enum InversionX { Disabled = 0, Enabled = 1 };
+    public enum InversionY { Disabled = 0, Enabled = 1 };
+    public enum Smooth { Disabled = 0, Enabled = 1 };
+
+    [Header("General")]
+    [Tooltip("Mouse sensitivity")]
+    public float sensitivity = 2;
+    [Tooltip("Distance to observations object")]
+    public float distance = 5;
+    [Tooltip("Height of camera")]
+    public float height = 2.3f;
+
+    [Header("Over the shoulder")]
+    public float offsetPosition;
+
+    [Header("Clamp angle")]
+    public float minY = 45f;
+    public float maxY = 45f;
+
+    [Header("Invert")]
+    public InversionX inversionX = InversionX.Disabled;
+    public InversionY inversionY = InversionY.Disabled;
+
+    [Header("Smooth Movement")]
+    public Smooth smooth = Smooth.Enabled;
+    public float speed = 8;
 }
